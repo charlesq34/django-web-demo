dc7e54a45e5aa2c4f6837a16d479a1664c043508
rqi <rqi@stanford.edu>
2015-08-04 15:20:31 -0700

    supported parallel access

--- visiondemo/external_app.py
+++ visiondemo/external_app.py
@@ -4,21 +4,36 @@ import ImageFilter
 import time
 import glob
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+print BASE_DIR
+print os.path.join(BASE_DIR, 'media/%s_tmp.jpg' % ("abcdefg"))
+
+def process_image(qfname):
+    try:
+        tag = qfname.split('/')[-1].strip('.query.done')
+        # IMAGE PROCESSING... BEG
+        print os.path.join(BASE_DIR, 'media/%s_tmp.jpg' % (tag))
+        im = Image.open(os.path.join(BASE_DIR, 'media/%s_tmp.jpg' % (tag)))
+        im = im.filter(ImageFilter.BLUR)
+        im.save(os.path.join(BASE_DIR, 'media/%s_tmp_out.jpg' % (tag)))
+        print 'finish processing %s ' % (tag)
+        # IMAGE PROCESSING... END
+        open(os.path.join(BASE_DIR, 'media/%s.result.done' % (tag)),'w').close()
+    except:
+        pass
+    os.remove(qfname)
 
 while 1:
     try:
-        while len(glob.glob('media/*.query.done')) == 0:
-            time.sleep(1)
-            pass
-        im = Image.open('media/tmp.jpg')
-        im = im.filter(ImageFilter.BLUR)
-        im.save('media/tmp_out.jpg')
-        fname = glob.glob('media/*.query.done')
-        print fname
-        fname = fname[0].strip('.query.done')
-        open(fname+'.result.done','w').close()
-        qfnames = glob.glob('media/*.query.done')
-        for f in qfnames:
-            os.remove(f)
+        while 1:
+            qfnames = glob.glob(os.path.join(BASE_DIR, 'media/*.query.done'))
+            if len(qfnames) == 0:
+                time.sleep(1)
+                pass
+            else:
+                break
+        print qfnames
+        for qfname in qfnames:
+            process_image(qfname)
     except:
         pass
--- visiondemo/views.py
+++ visiondemo/views.py
@@ -2,10 +2,11 @@ import os
 import random
 import string
 import time
+import uuid
 
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext, loader
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
@@ -13,12 +14,23 @@ from .forms import URLForm, UploadFileForm
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Create your views here.
+# MAIN PAGE
 def index(request):
     return render(request, 'visiondemo/demo.html')  
 
-def handle_uploaded_file(f):
-    with open(os.path.join(BASE_DIR, 'media/tmp.jpg'), 'wb+') as dst:
+# RESULT PAGE
+def result(request):
+    if 'cookie_tag' in request.COOKIES:
+        tag = request.COOKIES[ 'cookie_tag' ]
+        print('cookie_tag: %s' % (tag))
+        img_fname = '../media/%s_tmp_out.jpg' % (tag)
+        return render(request, 'visiondemo/result.html', {'image_list': [img_fname]})
+    else:
+        return Http404("Error")
+
+
+def handle_uploaded_file(f, fname):
+    with open(fname, 'wb+') as dst:
         for chunk in f.chunks():
             dst.write(chunk)
 
@@ -26,48 +38,50 @@ def loadFromExternalApp(filepath):
     while not os.path.exists(filepath+'.done'):
         time.sleep(1)
         pass
+    print('found result...')
     return 1 #json.load(open(filepath))
 
-# TODO: make sure there is a valid image before submit. retrieve result with request session id.
-#@csrf_exempt
-def result(request):
-    #return HttpResponse("Hello, world. You've succeeded in uploading the image.")
-    N = 10
-    tag = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
+def dispatch_job(tag):
     open(os.path.join(BASE_DIR, 'media/', tag+'.query.done'), 'w').close()
+    return loadFromExternalApp(os.path.join(BASE_DIR, 'media/', tag+'.result'))
 
-    result = loadFromExternalApp(os.path.join(BASE_DIR, 'media/', tag+'.result'))
-    return render(request, 'visiondemo/result.html')
-
+# HANDLE FILE UPLOAD
 @csrf_exempt
 def fileupload(request):
+
+    #N = 10
+    #tag = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
+    tag = str(uuid.uuid4())
+    fname = os.path.join(BASE_DIR, 'media/%s_tmp.jpg' % (tag))
+    print('fileupload tag: %s' % (tag))
+
+    response = HttpResponseRedirect(reverse('visiondemo:result'))
+    response.set_cookie( 'cookie_tag', tag )
+
+
     if request.method == 'POST':
-        #try:
-        #    handle_uploaded_file(request.FILES['file'])
-        #    HttpResponse('ok')
-        #except:
-        #    pass
+        # FROM DROPZONE UPLOAD
         try:
             print('try UploadFileForm ...')
             form = UploadFileForm(request.POST, request.FILES)
             print(form.is_valid())
             if form.is_valid():
-                handle_uploaded_file(request.FILES['file'])
-                time.sleep(3)
-                return HttpResponseRedirect(reverse('visiondemo:result'))
+                handle_uploaded_file(request.FILES['file'], fname)
+                dispatch_job(tag)
+                return response
         except:
             pass
-
+        # FROM URL TEXT
         try:
             print('URL form...')
             form = URLForm(request.POST)
             print(form)
             if form.is_valid():
                 print(form.cleaned_data['myinput'])
-                os.system('wget %s -O %s' % (form.cleaned_data['myinput'], os.path.join(BASE_DIR, 'media/tmp.jpg')))
-                time.sleep(3)
-                return HttpResponseRedirect(reverse('visiondemo:result'))
+                os.system('wget %s -O %s' % (form.cleaned_data['myinput'], fname))
+                dispatch_job(tag)
+                return response
         except:
             pass
 
-    return HttpResponse('error')
+    return Http404("Error")
