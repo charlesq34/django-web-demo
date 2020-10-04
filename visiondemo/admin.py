8cdaca075a2309ecc92f70b9439b22484d4d35ef
William Alexander <william.al1379@gmail.com>
2020-10-03 20:59:50 -0600

    Implement new Model person

--- visiondemo/admin.py
+++ visiondemo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Person
+
+class PersonAdmin(admin.ModelAdmin):
+    search_fields = ['first_name']
+    pass
+
+admin.site.register(Person, PersonAdmin)
--- visiondemo/models.py
+++ visiondemo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.first_name + " "+ self.last_name
+    
